25897cb35e4748fbbbb6e509fe074cee388fa147
Ajuka David <ajuka.zephiniah@gmail.com>
2021-04-28 00:26:14 +0100

    files without node_modules folder

---
+++ src/components/Task.vue
@@ -0,0 +1,39 @@
+<template>
+<div @dblclick="$emit('toggleReminder', task.id)" :class="[task.reminder ? 'reminder' : ' ', 'task']">
+     <h3>
+        {{task.text}}
+        <i @click="$emit('deleteTask', task.id)" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+</div>
+   
+</template>
+
+<script>
+export default  {
+    name: 'Task',
+    props: {
+        task: Object
+    }
+    
+}
+</script>
+<style scopeed>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f47c;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
